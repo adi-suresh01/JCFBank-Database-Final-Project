a47c463ea6060371aad08db8560313ced26aef47
adi-suresh01 <152752311+adi-suresh01@users.noreply.github.com>
2023-12-22 17:49:23 -0500

    orm

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+from sqlalchemy.ext.automap import automap_base
 import mysql.connector
 
 app = Flask(__name__)
@@ -13,6 +14,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+app.app_context().push()
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
+client = Base.classes.client
+transaction = Base.classes.transaction
 
 app.app_context().push()
 
@@ -29,6 +36,67 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+# class client(db.model):
+#     __tablename__ = 'client2'
+#     ssn = db.column('ssn', db.string(11), primary_key=True, unique=True)
+#     fname = db.column('Fname', db.string(50))
+#     lname = db.column('Lname', db.string(50))
+#     dob = db.column('DOB', db.date)
+#     email = db.column('email', db.string(50))
+#     phone = db.column('phone', db.string(255))
+#     street = db.column('street', db.string(255))
+#     city = db.column('city', db.string(255))
+#     state = db.column('state', db.string(255))
+#
+#     def __init__(self, ssn, fname, lname, dob, email, phone, street, city, state):
+#         self.ssn = ssn
+#         self.fname = fname
+#         self.lname = lname
+#         self.dob = dob
+#         self.email = email
+#         self.phone = phone
+#         self.street = street
+#         self.city = city
+#         self.state = state
+
+
+class TransForm(FlaskForm):
+    acc_no = StringField(validators=[
+        InputRequired()], render_kw={"placeholder": "Enter Your Account Number"})
+    amount = IntegerField(validators=[InputRequired()], render_kw={"placeholder", "Enter Amount to be transferred"})
+    to_acc = StringField(validators=[InputRequired()], render_kw={"placeholder", "Enter Account number you'd like to transfer to"})
+
+
+class ClientForm(FlaskForm):
+    ssn = StringField(validators=[
+                           InputRequired(), Length(min=11, max=11)], render_kw={"placeholder": "ssn"})
+
+    fname = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "First Name"})
+    lname = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "Last Name"})
+    dob = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "Date of birth"})
+    email = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "email"})
+    phone = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "phone"})
+    street = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "street address"})
+    city = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "city"})
+    state = StringField(validators=[
+                             InputRequired(), Length(min=3, max=50)], render_kw={"placeholder": "state"})
+
+    submit = SubmitField('Submit')
+
+    # def validate_client(self, ssn):
+    #     existing_client = client.query.filter_by(
+    #         ssn=ssn.data).first()
+    #     if existing_client:
+    #         raise ValidationError(
+    #             'Existing Client')
 
 
 class RegisterForm(FlaskForm):
@@ -86,6 +154,25 @@ def logout():
     return redirect(url_for('login'))
 
 
+
+@app.route('/client', methods=['GET', 'POST'])
+def new_client():
+    form = ClientForm()
+
+    if form.validate_on_submit():
+        new_client = client(ssn=form.ssn.data, Fname=form.fname.data, Lname=form.lname.data, DOB=form.dob.data, email=form.email.data, phone=form.phone.data, street=form.street.data, city=form.city.data, state=form.state.data)
+        db.session.add(new_client)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+    return render_template('client.html', form=form)
+
+# (ssn=form.ssn.data, fname=form.fname.data, lname=form.lname.data, dob=form.dob.data, email=form.email.data, phone=form.phone.data, street=form.street.data, city=form.city.data, state=form.state.data)
+
+# @app.route('/transaction', methods=['GET', 'POST'])
+# def transaction():
+#     form = Transaction.form()
+#      if form.validate_on_submit():
+#          new_transaction = transaction()
 
 
 @ app.route('/register', methods=['GET', 'POST'])
